70910d290298a9aa3795eb063b51439b40feeb10
Justwait <justwait@method.gg>
2017-12-06 18:40:39 +0100

    Upload some files

---
+++ Felhounds.lua
@@ -0,0 +1,244 @@
+
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Felhounds of Sargeras", nil, 1987, 1712)
+if not mod then return end
+mod:RegisterEnableMob(122477, 122135) -- F'harg, Shatug
+mod.engageId = 2074
+mod.respawnTime = 29
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local moltenTouchCount = 0
+local enflameCorruptionCount = 0
+local siphonCorruptionCount = 0
+local weightofDarknessCount = 0
+local consumingSphereCount = 0
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		--[[ F'harg ]]--
+		251445, -- Burning Maw
+		244072, -- Molten Touch
+		{244768, "SAY"}, -- Desolate Gaze
+		244057, -- Enflame Corruption
+		{248815, "SAY"}, -- Enflamed
+
+		--[[ Shatug ]]--
+		245098, -- Corrupting Maw
+		244131, -- Consuming Sphere
+		{254429, "SAY"}, -- Weight of Darkness
+		244056, -- Siphon Corruption
+		{248819, "SAY"}, -- Siphoned
+
+		--[[ General ]]--
+		244050, -- Destroyer's Boon
+		251356, -- Focusing Power
+	},{
+		[251445] = -15842, -- F'harg
+		[245098] = -15836, -- Shatug
+		[244050] = "general",
+	}
+end
+
+function mod:OnBossEnable()
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1", "boss2")
+
+	--[[ F'harg ]]--
+	self:Log("SPELL_CAST_SUCCESS", "BurningMaw", 251445)
+	self:Log("SPELL_AURA_APPLIED", "MoltenTouchApplied", 244072)
+	self:Log("SPELL_AURA_APPLIED", "DesolateGazeApplied", 244768)
+	self:Log("SPELL_AURA_REMOVED", "DesolateGazeRemoved", 244768)
+	self:Log("SPELL_CAST_SUCCESS", "EnflameCorruption", 244057)
+	self:Log("SPELL_AURA_APPLIED", "Enflamed", 248815)
+	self:Log("SPELL_AURA_REMOVED", "EnflamedRemoved", 248815)
+
+	--[[ Shatug ]]--
+	self:Log("SPELL_CAST_SUCCESS", "CorruptingMaw", 245098)
+	self:Log("SPELL_AURA_APPLIED", "WeightofDarknessApplied", 254429)
+	self:Log("SPELL_AURA_REMOVED", "WeightofDarknessRemoved", 254429)
+	self:Log("SPELL_CAST_SUCCESS", "SiphonCorruption", 244056)
+	self:Log("SPELL_AURA_APPLIED", "Siphoned", 248819)
+	self:Log("SPELL_AURA_REMOVED", "SiphonedRemoved", 248819)
+
+	--[[ General ]]--
+	self:Log("SPELL_AURA_APPLIED", "SargerasBlessing", 246057) -- Destroyer's Boon buff
+	self:Log("SPELL_AURA_APPLIED", "FocusingPower", 251356)
+end
+
+function mod:OnEngage()
+	moltenTouchCount = 1
+	enflameCorruptionCount = 1
+	siphonCorruptionCount = 1
+	weightofDarknessCount = 1
+	consumingSphereCount = 1
+
+	self:CDBar(251445, self:Mythic() and 10.9 or 10.5) -- Burning Maw
+	self:CDBar(245098, self:Mythic() and 10.9 or 10.5) -- Corrupting Maw
+	self:Bar(244056, self:Mythic() and 26.5 or self:Easy() and 29 or 28) -- Siphon Corruption
+	self:Bar(244057, self:Mythic() and 49.6 or self:Easy() and 56 or 52) -- Enflame Corruption
+	self:Bar(244131, self:Mythic() and 49 or self:Easy() and 54.5 or 52.5) -- Consuming Sphere
+	self:Bar(244768, self:Mythic() and 77.5 or self:Easy() and 89 or 84.5) -- Desolate Gaze
+
+	if not self:Easy() then
+		self:Bar(244072, self:Mythic() and 18 or 20) -- Molten Touch
+		self:Bar(254429, self:Mythic() and 73 or 78) -- Weight of Darkness
+	end
+	if self:Mythic() then
+		self:Bar(251356, 120) -- Focusing Power
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName, _, _, spellId)
+	if spellId == 244159 then -- Consuming Sphere
+		self:Message(244131, "Attention", "Alert")
+		consumingSphereCount = consumingSphereCount + 1
+		self:Bar(244131, self:Mythic() and (consumingSphereCount % 2 == 1 and 86 or 72.5) or self:Easy() and 85 or 78.5)
+	elseif spellId == 244069 then -- Weight of Darkness
+		weightofDarknessCount = weightofDarknessCount + 1
+		self:Bar(254429, self:Mythic() and (weightofDarknessCount % 2 == 1 and 72.5 or 86) or 78.5)
+	elseif spellId == 244064 then -- Desolate Gaze
+		self:Bar(244768, self:Mythic() and 103 or self:Easy() and 104 or 96.5)
+	end
+end
+
+function mod:BurningMaw(args)
+	self:Message(args.spellId, "Important", "Alarm")
+	self:CDBar(args.spellId, self:Mythic() and 10.9 or 11)
+end
+
+do
+	local playerList = mod:NewTargetList()
+	function mod:MoltenTouchApplied(args)
+		playerList[#playerList+1] = args.destName
+		if #playerList == 1 then
+			self:ScheduleTimer("TargetMessage", 0.3, args.spellId, playerList, "Attention", "Warning")
+			moltenTouchCount = moltenTouchCount + 1
+			self:Bar(args.spellId, self:Mythic() and (moltenTouchCount % 2 == 1 and 103.3 or 88.8) or 96.5)
+		end
+	end
+end
+
+do
+	local playerList = mod:NewTargetList()
+	function mod:DesolateGazeApplied(args)
+		if self:Me(args.destGUID) then
+			self:Say(args.spellId)
+			self:SayCountdown(args.spellId, 8)
+		end
+		playerList[#playerList+1] = args.destName
+		if #playerList == 1 then
+			self:ScheduleTimer("TargetMessage", 0.3, args.spellId, playerList, "Urgent", "Warning", nil, nil, true)
+		end
+	end
+end
+
+function mod:DesolateGazeRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CancelSayCountdown(args.spellId)
+	end
+end
+
+function mod:EnflameCorruption(args)
+	self:Message(args.spellId, "Attention", "Alert")
+	enflameCorruptionCount = enflameCorruptionCount + 1
+	self:Bar(args.spellId, self:Mythic() and (enflameCorruptionCount % 2 == 1 and 88.8 or 104.6) or self:Easy() and 104 or 95.5)
+	self:CastBar(args.spellId, 9)
+end
+
+function mod:Enflamed(args)
+	if self:Me(args.destGUID) then
+		self:TargetMessage(args.spellId, args.destName, "Personal", "Warning")
+		self:Say(args.spellId)
+		self:SayCountdown(args.spellId, 4)
+	end
+end
+
+function mod:EnflamedRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CancelSayCountdown(args.spellId)
+	end
+end
+
+function mod:CorruptingMaw(args)
+	self:Message(args.spellId, "Important", "Alarm")
+	self:CDBar(args.spellId, self:Mythic() and 10.9 or 11)
+end
+
+do
+	local playerList = mod:NewTargetList()
+	function mod:WeightofDarknessApplied(args)
+		if self:Me(args.destGUID) then
+			self:Say(args.spellId)
+			self:SayCountdown(args.spellId, 5)
+		end
+		playerList[#playerList+1] = args.destName
+		if #playerList == 1 then
+			self:ScheduleTimer("TargetMessage", 0.3, args.spellId, playerList, "Urgent", "Warning", nil, nil, true)
+		end
+	end
+end
+
+function mod:WeightofDarknessRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CancelSayCountdown(args.spellId)
+	end
+end
+
+function mod:SiphonCorruption(args)
+	self:Message(args.spellId, "Attention", "Alert")
+	siphonCorruptionCount = siphonCorruptionCount + 1
+	self:Bar(args.spellId, self:Mythic() and (siphonCorruptionCount % 2 == 1 and 86.3 or 73) or self:Easy() and 85 or 78.5)
+	self:CastBar(args.spellId, 9)
+end
+
+function mod:Siphoned(args)
+	if self:Me(args.destGUID) then
+		self:TargetMessage(args.spellId, args.destName, "Personal", "Warning")
+		self:SayCountdown(args.spellId, 4)
+	end
+end
+
+function mod:SiphonedRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CancelSayCountdown(args.spellId)
+	end
+end
+
+do
+	local prev = 0
+	function mod:SargerasBlessing(args)
+		local t = GetTime()
+		if t-prev > 0.5 then
+			prev = t
+			self:Message(244050, "Urgent", "Warning", args.spellName, args.spellId)
+		end
+	end
+end
+
+do
+	local prev = 0
+	function mod:FocusingPower(args)
+		local t = GetTime()
+		if t-prev > 0.5 then
+			prev = t
+			self:Message(args.spellId, "Neutral", "Info")
+			self:CastBar(args.spellId, 15)
+			if self:Mythic() then -- Changes in Mythic
+				self:Bar(args.spellId, 120)
+			end
+		end
+	end
+end
---
+++ Kingaroth.lua
@@ -0,0 +1,331 @@
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Kin'garoth", nil, 2004, 1712)
+if not mod then return end
+mod:RegisterEnableMob(122578)
+mod.engageId = 2088
+mod.respawnTime = 30
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local nextApocalypseProtocol = 0
+local mobTable = {
+	[123906] = {}, -- Garothi Annihilator
+	[123929] = {}, -- Garothi Demolisher
+	[123921] = {}, -- Garothi Decimator
+}
+local mobCount = {
+	[123906] = 0, -- Garothi Annihilator
+	[123929] = 0, -- Garothi Demolisher
+	[123921] = 0, -- Garothi Decimator
+}
+
+local empBomb = nil
+local empRuiner = nil
+local empStrike = nil
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:GetLocale()
+if L then
+	L.empowered = "(E) %s" -- (E) Ruiner
+	L.gains = "Kin'garoth gains %s"
+end
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		--[[ Stage: Deployment ]]--
+		{254919, "TANK"}, -- Forging Strike
+		{254926, "SAY", "FLASH"}, -- Reverberating Strike
+		248214, -- Diabolic Bomb
+		246833, -- Ruiner
+		248375, -- Shattering Strike
+
+		--[[ Stage: Construction ]]--
+		246516, -- Apocalypse Protocol
+
+		--[[ Adds ]]--
+		246664, -- Annihilation
+		246686, -- Decimation
+		{246698, "SAY"}, -- Demolish
+
+		--[[ Mythic ]]--
+		{249680, "SAY"}, -- Reverberating Decimation
+	},{
+		[244312] = -16151, -- Stage: Deployment
+		[246516] = -16152, -- Stage: Construction
+		[246664] = -16143, -- Garothi Annihilator
+		[246686] = -16144, -- Garothi Decimator
+		[246698] = -16145, -- Garothi Demolisher
+		[249680] = "mythic",
+	}
+end
+
+function mod:OnBossEnable()
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1", "boss2", "boss3", "boss4")
+
+	--[[ Stage: Deployment ]]--
+	self:Log("SPELL_CAST_START", "ForgingStrike", 254919)
+	self:Log("SPELL_AURA_APPLIED", "ForgingStrikeApplied", 254919)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "ForgingStrikeApplied", 254919)
+	self:Log("SPELL_CAST_START", "ReverberatingStrike", 254926)
+	self:Log("SPELL_CAST_SUCCESS", "DiabolicBomb", 248214)
+	self:Log("SPELL_CAST_START", "Ruiner", 246833)
+
+	--[[ Stage: Construction ]]--
+	self:Log("SPELL_AURA_APPLIED", "ApocalypseProtocol", 246516)
+	self:Log("SPELL_AURA_REMOVED", "ApocalypseProtocolOver", 246516)
+
+	--[[ Adds ]]--
+	self:Log("SPELL_CAST_SUCCESS", "Initializing", 246504)
+	self:Log("SPELL_AURA_APPLIED", "Decimation", 246687)
+	self:Log("SPELL_AURA_APPLIED", "Demolish", 246698)
+	self:Log("SPELL_CAST_SUCCESS", "DemolishSuccess", 246692)
+	self:Death("AddDeaths", 123906, 123929, 123921) -- Garothi Annihilator, Garothi Demolisher, Garothi Decimator
+
+	--[[ Mythic ]]--
+	self:Log("SPELL_AURA_APPLIED", "EmpoweredReverberatingStrike", 254795)
+	self:Log("SPELL_AURA_APPLIED", "EmpoweredDiabolicBomb", 254796)
+	self:Log("SPELL_AURA_APPLIED", "EmpoweredRuiner", 254797)
+
+	self:Log("SPELL_AURA_APPLIED", "ReverberatingDecimation", 249680)
+end
+
+function mod:OnEngage()
+	mobTable = {
+		[123906] = {}, -- Garothi Annihilator
+		[123929] = {}, -- Garothi Demolisher
+		[123921] = {}, -- Garothi Decimator
+	}
+	mobCount = {
+		[123906] = 0, -- Garothi Annihilator
+		[123929] = 0, -- Garothi Demolisher
+		[123921] = 0, -- Garothi Decimator
+	}
+	empBomb = nil
+	empRuiner = nil
+	empStrike = nil
+
+	self:Bar(254919, 5.5) -- Forging Strike
+	self:Bar(248214, 12.5) -- Diabolic Bomb
+	self:Bar(254926, 14.5) -- Reverberating Strike
+	self:Bar(246833, 25) -- Ruiner
+
+	nextApocalypseProtocol = GetTime() + 37.5
+	self:Bar(246516, 37.5) -- Apocalypse Protocol
+end
+
+--------------------------------------------------------------------------------
+-- Local Functions
+--
+
+local function getMobNumber(mobId, guid)
+	if mobTable[mobId][guid] then return mobTable[mobId][guid] end
+	mobCount[mobId] = mobCount[mobId] + 1
+	mobTable[mobId][guid] = mobCount[mobId]
+	return mobCount[mobId]
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(unit, spellName, _, _, spellId)
+	if spellId == 246686 then -- Decimation
+		local guid = UnitGUID(unit)
+		local mobId = self:MobId(guid)
+		local mobText = getMobNumber(mobId, guid)
+		self:Bar(spellId, 15.8, CL.count:format(spellName, mobText))
+	elseif spellId == 246657 then -- Annihilation
+		local guid = UnitGUID(unit)
+		local mobId = self:MobId(guid)
+		local mobText = getMobNumber(mobId, guid)
+		self:Message(246664, "Important", "Alarm")
+		self:Bar(246664, 15.8, CL.count:format(spellName, mobText))
+	elseif spellId == 248375 then -- Shattering Strike
+		self:Message(spellId, "Urgent", "Warning")
+	end
+end
+
+--[[ Stage: Deployment ]]--
+function mod:ForgingStrike(args)
+	self:Message(args.spellId, "Attention", "Alert")
+	local cooldown = 14.5
+	if nextApocalypseProtocol > GetTime() + cooldown then
+		self:CDBar(args.spellId, cooldown)
+	end
+end
+
+function mod:ForgingStrikeApplied(args)
+	local amount = args.amount or 1
+	self:StackMessage(args.spellId, args.destName, amount, "Urgent", "Warning")
+end
+
+do
+	local function printTarget(self, name, guid)
+		self:TargetMessage(254926, name, "Attention", "Alert", nil, nil, true)
+		if self:Me(guid) then
+			self:Say(254926)
+			self:Flash(254926)
+		end
+	end
+
+	function mod:ReverberatingStrike(args)
+		self:GetBossTarget(printTarget, 0.5, args.sourceGUID)
+		local cooldown = 30
+		if nextApocalypseProtocol > GetTime() + cooldown then
+			self:CDBar(args.spellId, cooldown, empStrike and L.empowered:format(args.spellName))
+		end
+	end
+end
+
+function mod:DiabolicBomb(args)
+	self:Message(args.spellId, "Important", "Alarm")
+	local cooldown = 20.5
+	if nextApocalypseProtocol > GetTime() + cooldown then
+		if empBomb then -- Empowered
+			self:CDBar(args.spellId, cooldown, L.empowered:format(args.spellName))
+		else
+			self:CDBar(args.spellId, cooldown)
+		end
+	end
+end
+
+function mod:Ruiner(args)
+	self:Message(args.spellId, "Urgent", "Warning", CL.casting:format(args.spellName))
+	local cooldown = 30
+	if nextApocalypseProtocol > GetTime() + cooldown then
+		if empRuiner then -- Empowered
+			self:CDBar(args.spellId, cooldown, L.empowered:format(args.spellName))
+		else
+			self:CDBar(args.spellId, cooldown)
+		end
+	end
+end
+
+--[[ Stage: Construction ]]--
+function mod:ApocalypseProtocol(args)
+	self:StopBar(254919) -- Forging Strike
+	self:StopBar(248214) -- Diabolic Bomb
+	self:StopBar(254926) -- Reverberating Strike
+	self:StopBar(246833) -- Ruiner
+	self:StopBar(L.empowered:format(self:SpellName(248214))) -- (E) Diabolic Bomb
+	self:StopBar(L.empowered:format(self:SpellName(254926))) -- (E) Reverberating Strike
+	self:StopBar(L.empowered:format(self:SpellName(246833))) -- (E) Ruiner
+	self:Message(args.spellId, "Positive", "Long")
+	self:CastBar(args.spellId, 40)
+	nextApocalypseProtocol = GetTime() + 120
+	self:Bar(args.spellId, 120)
+end
+
+function mod:ApocalypseProtocolOver(args)
+	self:Message(args.spellId, "Neutral", "Info", CL.over:format(args.spellName))
+	self:Bar(248214, 3, empBomb and L.empowered:format(self:SpellName(248214))) -- Diabolic Bomb
+	self:Bar(254919, 7.5) -- Forging Strike
+	self:Bar(246833, 12.5, empRuiner and L.empowered:format(self:SpellName(246833))) -- Ruiner
+	self:Bar(254926, 24.5, empStrike and L.empowered:format(self:SpellName(254926))) -- Reverberating Strike
+end
+
+--[[ Adds ]]--
+function mod:Initializing(args)
+	local mobId = self:MobId(args.sourceGUID)
+	local mobText = getMobNumber(mobId, args.sourceGUID)
+	if mobId == 123906 then -- Garothi Annihilator
+		self:CDBar(246664, 45, CL.count:format(self:SpellName(246664), mobText)) -- Annihilation
+	elseif mobId == 123929 then -- Garothi Demolisher
+		self:CDBar(246698, 47.5, CL.count:format(self:SpellName(246698), mobText)) -- Demolish
+	elseif mobId == 123921 then -- Garothi Decimator
+		self:CDBar(246686, 45, CL.count:format(self:SpellName(246686), mobText)) -- Decimation
+	end
+end
+
+do
+	local playerList = mod:NewTargetList()
+	function mod:Demolish(args)
+		if self:Me(args.destGUID) then
+			self:Say(args.spellId)
+			self:SayCountdown(args.spellId, 6)
+		end
+		playerList[#playerList+1] = args.destName
+		if #playerList == 1 then
+			self:ScheduleTimer("TargetMessage", 0.5, args.spellId, playerList, "Urgent", "Warning")
+		end
+	end
+
+	function mod:DemolishSuccess(args)
+		local mobId = self:MobId(args.sourceGUID)
+		local mobText = getMobNumber(mobId, args.sourceGUID)
+		self:Bar(246698, 15.8, CL.count:format(args.spellName, mobText))
+	end
+end
+
+do
+	local playerList = mod:NewTargetList()
+	function mod:Decimation(args)
+		if self:Me(args.destGUID) then
+			self:Say(246686)
+			self:SayCountdown(246686, 6)
+		end
+		playerList[#playerList+1] = args.destName
+		if #playerList == 1 then
+			self:ScheduleTimer("TargetMessage", 0.3, 246686, playerList, "Urgent", "Warning")
+		end
+	end
+end
+
+function mod:AddDeaths(args)
+	local mobText = getMobNumber(args.mobId, args.destGUID)
+	if args.mobId == 123906 then -- Garothi Annihilator
+		self:StopBar(CL.count:format(self:SpellName(246664), mobText)) -- Annihilation
+	elseif args.mobId == 123929 then -- Garothi Demolisher
+		self:StopBar(CL.count:format(self:SpellName(246698), mobText)) -- Demolish
+	elseif args.mobId == 123921 then -- Garothi Decimator
+		self:StopBar(CL.count:format(self:SpellName(246686), mobText)) -- Decimation
+	end
+end
+
+--[[ Mythic ]]--
+function mod:EmpoweredRuiner(args)
+	self:Message(246833, "Neutral", "Info", L.gains:format(args.spellName))
+	empRuiner = true
+	self:Bar(246833, self:BarTimeLeft(self:SpellName(246833)), L.empowered:format(self:SpellName(246833))) -- (E) Ruiner
+	self:StopBar(246833)
+end
+
+function mod:EmpoweredReverberatingStrike(args)
+	self:Message(254926, "Neutral", "Info", L.gains:format(args.spellName))
+	empStrike = true
+	self:Bar(254926, self:BarTimeLeft(self:SpellName(254926)), L.empowered:format(self:SpellName(254926))) -- (E) Reverberating Strike
+	self:StopBar(254926)
+end
+
+function mod:EmpoweredDiabolicBomb(args)
+	self:Message(248214, "Neutral", "Info", L.gains:format(args.spellName))
+	empBomb = true
+	self:Bar(248214, self:BarTimeLeft(self:SpellName(248214)), L.empowered:format(self:SpellName(248214))) -- (E) Diabolic Bomb
+	self:StopBar(248214)
+end
+
+do
+	local playerList = mod:NewTargetList()
+	function mod:ReverberatingDecimation(args)
+		if self:Me(args.destGUID) then
+			self:Say(args.spellId)
+			self:SayCountdown(args.spellId, 4)
+		end
+		playerList[#playerList+1] = args.destName
+		if #playerList == 1 then
+			self:ScheduleTimer("TargetMessage", 0.3, args.spellId, playerList, "Urgent", "Warning")
+		end
+	end
+end
